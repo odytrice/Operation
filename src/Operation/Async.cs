b4dde16aa1286d9a9f6a77489a14a4165ee8351c
Ody Mbegbu <odytrice@gmail.com>
2018-05-23 15:52:54 +0100

    Added Support for await keyword

---
+++ src/Operation/Async.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+
+    public static partial class OperationExtensions
+    {
+        public static OperationAwaiter<T> GetAwaiter<T>(this Operation<T> operation)
+        {
+            return new OperationAwaiter<T>(operation);
+        }
+
+        public static OperationAwaiter GetAwaiter(this Operation operation)
+        {
+            return new OperationAwaiter(operation);
+        }
+
+    }
+
+    public class OperationAwaiter : INotifyCompletion
+    {
+        private Operation _operation;
+
+        public OperationAwaiter(Operation operation)
+        {
+            _operation = operation;
+        }
+        public bool IsCompleted => true;
+
+        public void OnCompleted(Action continuation)
+        {
+            continuation();
+        }
+
+        public void GetResult()
+        {
+            _operation.Unwrap();
+        }
+    }
+
+
+    public class OperationAwaiter<T> : INotifyCompletion
+    {
+        private Operation<T> _operation;
+
+        public OperationAwaiter(Operation<T> operation)
+        {
+            _operation = operation;
+        }
+        public bool IsCompleted => true;
+
+        public void OnCompleted(Action continuation)
+        {
+            continuation();
+        }
+
+        public T GetResult()
+        {
+            return _operation.Unwrap();
+        }
+    }
+}
---
+++ test/Tests/AsyncTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public class AsyncTests
+    {
+        [TestMethod]
+        public async Task AwaitSuccessfulOperation()
+        {
+            var operation = Operation.Success(10);
+
+            var result = await operation;
+
+            Assert.AreEqual(10, result);
+        }
+
+        [TestMethod]
+        public async Task AwaitFailedOperation()
+        {
+            var operation = Operation.Fail<int>("An Error Occured");
+
+            bool failed = false;
+            try
+            {
+                var result = await operation;
+            }
+            catch
+            {
+                failed = true;
+            }
+
+            Assert.IsTrue(failed, "Operation Not Throwing Exception");
+        }
+
+        [TestMethod]
+        public void TestErrorCapturedInTask()
+        { 
+            var result = AsyncOpMethod();
+
+            Assert.IsTrue(result.IsFaulted);
+
+            async Task<int> AsyncOpMethod()
+            {
+                await Operation.Fail<int>("An Error Occured");
+                return 1;
+            }
+        }
+
+        [TestMethod]
+        public async Task AwaitNonGenericOperation()
+        {
+            var operation = Operation.Success();
+            await operation;
+        }
+    }
+}
